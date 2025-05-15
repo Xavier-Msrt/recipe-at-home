717edfa8aaf6548bff38c5b9ad112af69c8d7322
Xavier-Msrt <xaviermassart@outlook.com>
2025-05-15 22:04:52 +0200

    feat: save picture + clean image

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,79 +1,116 @@
 import sql from "@/lib/data";
 import { Ingredient } from "@/types/Ingredient";
-import { Recipe } from '@/types/Recipe'
+import { Recipe } from "@/types/Recipe";
 import { Step } from "@/types/Step";
-import { error } from "console";
+import fs from "fs";
+import path from "path";
 import { NextRequest, NextResponse } from "next/server";
 
+const UPLOAD_DIR = path.resolve(process.env.ROOT_PATH ?? "", "public/uploads");
+
 export async function GET() {
-    return new Response(JSON.stringify(await sql<Recipe[]>`SELECT * FROM recipeathome.recipes;`));
+	return new Response(
+		JSON.stringify(await sql<Recipe[]>`SELECT * FROM recipeathome.recipes;`)
+	);
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    const data = await req.formData();
+	const data = await req.formData();
+	const body = Object.fromEntries(data);
 
-    const detail = checkDetail(data);
-    const ingredients = checkIngredients(data);
-    const steps = checkSteps(data);
+	const detail = checkDetail(body.recipe);
+	const ingredients = checkIngredients(body.ingredient);
+	const steps = checkSteps(body.step);
 
-    const picture = data.get('picture');
-    if(!picture) {
-        return NextResponse.json({error: 'picture missing'}, {status: 400});
-    }
 
-    // SAVE picture
-    // DB SAVE
+	// file upload
+	const file = (body.picture as Blob) || null;
+	
+	if (!file) {
+		return NextResponse.json({ error: "Picture is required" }, { status: 400 });
+	}
+	savePicture(file, "123.jpg")
 
-    return new Response();
+	// DB SAVE
+
+	return new Response();
 }
 
-function checkDetail(data: FormData) {
-    const recipe = data.get('recipe');
-    if(!recipe){
-        return NextResponse.json({error: 'recipe detail missing'}, {status: 400});
-    }
 
-    const recipeDetail: Recipe = JSON.parse(recipe.toString());
-    if(!recipeDetail.title ||
-        !recipeDetail.description
-    ){
-        return NextResponse.json({ error: 'title or description missing'}, {status: 400});
-    }
+async function savePicture(file: Blob, fileName: string){
+	
 
+	if (!file.type.startsWith("image/png") && !file.type.startsWith("image/jpeg")) {
+		return NextResponse.json({ error: "Invalid file type" }, { status: 400 });
+	}
 
-    return recipeDetail;
+	if (file.size > 5 * 1024 * 1024) { // 5MB limit
+		return NextResponse.json({ error: "File too large" }, { status: 400 });
+	}
+
+	//save pic
+	const buffer = Buffer.from(await file.arrayBuffer());
+	if (!fs.existsSync(UPLOAD_DIR)) {
+		fs.mkdirSync(UPLOAD_DIR);
+	}
+	fs.writeFileSync(path.resolve(UPLOAD_DIR, fileName), buffer);
+
 }
 
-function checkIngredients(data: FormData) {
-    const ingredients = data.get('ingredients');
-    if(!ingredients){
-        return NextResponse.json({error: 'ingredients missing'}, {status: 400});
-    }
 
-    const ingredientsObj: Ingredient[] = JSON.parse(ingredients.toString());
-    for (let i = 0; i < ingredientsObj.length; i++) {
-        const element: Ingredient = ingredientsObj[i];
-        if(!element.name || !element.quantity) {
-            return NextResponse.json({error: "ingredient " + i + " missing name or quantity"}, {status: 400});
-        }
-    }
+function checkDetail(recipe: FormDataEntryValue) {
+	if (!recipe) {
+		return NextResponse.json(
+			{ error: "recipe detail missing" },
+			{ status: 400 }
+		);
+	}
 
-    return ingredientsObj;
+	const recipeDetail: Recipe = JSON.parse(recipe.toString());
+	if (!recipeDetail.title || !recipeDetail.description) {
+		return NextResponse.json(
+			{ error: "title or description missing" },
+			{ status: 400 }
+		);
+	}
+
+	return recipeDetail;
 }
 
-function checkSteps(data: FormData) {
-    const steps = data.get('steps');
-    if(!steps){
-        return NextResponse.json({error: 'steps missing'}, {status: 400});
-    }
+function checkIngredients(ingredients: FormDataEntryValue) {
+	if (!ingredients) {
+		return NextResponse.json({ error: "ingredients missing" }, { status: 400 });
+	}
 
-    const stepsObj: Step[] = JSON.parse(steps.toString());
-    for (let i = 0; i < stepsObj.length; i++) {
-        const element: Step = stepsObj[i];
-        if(!element.description) {
-            return NextResponse.json({error: "steps " + i + " missing description"}, {status: 400});
-        }
-    }
+	const ingredientsObj: Ingredient[] = JSON.parse(ingredients.toString());
+	for (let i = 0; i < ingredientsObj.length; i++) {
+		const element: Ingredient = ingredientsObj[i];
+		if (!element.name || !element.quantity) {
+			return NextResponse.json(
+				{ error: "ingredient " + i + " missing name or quantity" },
+				{ status: 400 }
+			);
+		}
+	}
 
-    return stepsObj;    
+	return ingredientsObj;
+}
+
+function checkSteps(steps: FormDataEntryValue) {
+	if (!steps) {
+		return NextResponse.json({ error: "steps missing" }, { status: 400 });
+	}
+
+	const stepsObj: Step[] = JSON.parse(steps.toString());
+	for (let i = 0; i < stepsObj.length; i++) {
+		const element: Step = stepsObj[i];
+		if (!element.description) {
+			return NextResponse.json(
+				{ error: "steps " + i + " missing description" },
+				{ status: 400 }
+			);
+		}
+	}
+
+	return stepsObj;
 }
