328b3d084d9939208fedb4bebe2b3955e48a8430
Xavier-Msrt <xaviermassart@outlook.com>
2025-05-27 08:27:09 +0200

    chore: remove comment + prettier config

--- services/locale.ts
+++ services/locale.ts
@@ -3,9 +3,7 @@
 import { cookies } from 'next/headers';
 import { Locale, defaultLocale } from '@/i18n/config';
 
-// In this example the locale is read from a cookie. You could alternatively
-// also read it from a database, backend service, or any other source.
-const COOKIE_NAME = 'recipe-@-home';
+const COOKIE_NAME = 'recipe-at-home';
 
 export async function getUserLocale() {
     return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
