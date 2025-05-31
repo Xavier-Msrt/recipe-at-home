1ef5cfdd8e58cd6aa36a84e42fbd9c519c1b1bc9
Xavier-Msrt <xaviermassart@outlook.com>
2025-05-31 19:35:52 +0200

    feat: logo to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,9 @@ export default async function RootLayout({
                         <header>
                             <nav className="navbar bg-base-100 shadow-sm">
                                 <div className="flex-1">
-                                    <a className="btn btn-ghost text-xl">
+                                    <Link className="btn btn-ghost text-xl" href="/">
                                         {t('name')}
-                                    </a>
+                                    </Link>
                                 </div>
                                 <div className="flex-none">
                                     <ul className="menu menu-horizontal px-1">
